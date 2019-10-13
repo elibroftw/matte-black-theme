78d23a8856baba52027fa09006a4617baba00378
elibroftw <xxelibroxx@gmail.com>
2019-10-12 22:34:47 -0400

    Added new theme

--- zip.py
+++ zip.py
@@ -7,7 +7,7 @@ rmtree('Builds', ignore_errors=True)
 os.makedirs('Builds')
 
 
-build_folders = ['Matte Black (Blue) Theme Firefox', 'Matte Black (Grey) Theme Firefox', 'Matte Black (Orange) Theme Firefox', 'Matte Black (Red) Theme Firefox', 'Matte Black (Violet) Theme Firefox', 'Matte Black (White) Theme Firefox', 'Matte Black Theme Chrome', 'Dark Knight Joker Theme Firefox']
+build_folders = ['Matte Black (Pink) Theme Firefox', 'Matte Black (Blue) Theme Firefox', 'Matte Black (Grey) Theme Firefox', 'Matte Black (Orange) Theme Firefox', 'Matte Black (Red) Theme Firefox', 'Matte Black (Violet) Theme Firefox', 'Matte Black (White) Theme Firefox', 'Matte Black Theme Chrome', 'Dark Knight Joker Theme Firefox']
 for folder in build_folders:
     with ZipFile(f'Builds/{folder}.zip', 'w') as zf:
         for file in glob(f'{folder}/*'):
