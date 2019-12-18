8bb54d1a6eab1608cfb1a2100ad60cd1679364db
elibroftw <xxelibroxx@gmail.com>
2019-12-18 10:32:24 +0530

    Added theme + custom releases

--- zip.py
+++ zip.py
@@ -6,10 +6,10 @@ from glob import glob
 rmtree('Builds', ignore_errors=True)
 os.makedirs('Builds')
 
-
-build_folders = ['Matte Black (Cyan) Theme Firefox', 'Matte Black (Pink) Theme Firefox', 'Matte Black (Blue) Theme Firefox', 'Matte Black (Grey) Theme Firefox', 'Matte Black (Orange) Theme Firefox', 'Matte Black (Red) Theme Firefox', 'Matte Black (Violet) Theme Firefox', 'Matte Black (White) Theme Firefox', 'Dark Knight Joker Theme Firefox']
+colors = {'Cyan', 'Pink', 'Blue', 'Classic Blue', 'Grey', 'Orange', 'Red', 'Violet', 'White'}
+colors = {f'Matte Black ({color}) Theme Firefox' for color in colors}
+build_folders = {'Dark Knight Joker Theme Firefox'}.union(colors)
 for folder in build_folders:
-    
     with ZipFile(f'Builds/{folder}.zip', 'w') as zf:
         for file in glob(f'{folder}/*'):
             zf.write(f'{file}', os.path.basename(file))
