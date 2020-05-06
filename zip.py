8a2267f19c9c874c9ce08dfa1c996f4fbd00f16b
Elijah Lopez <elijahllopezz@gmail.com>
2020-05-06 16:17:51 -0400

    fixed bugs

--- zip.py
+++ zip.py
@@ -18,7 +18,9 @@ for folder in build_folders:
 chrome_folders = {'Matte Black Theme Chrome', 'Plexus Crystals Chrome'}
 for folder in chrome_folders:
     with ZipFile(f'Builds/{folder}.zip', 'w') as zf:
-        for file in glob(f'{folder}/*'):
-            zf.write(f'{file}', os.path.basename(file))
+        zf.write(f'{folder}/manifest.json', os.path.basename(file))
+        if os.path.exists(f'{folder}/images'):
+            for image in os.listdir(f'{folder}/images'):  # add images to archive
+                zf.write(f'{folder}/images/{image}', f'images/{image}')
         for icon in os.listdir(f'{folder}/icons'):  # add icons to archive
             zf.write(f'{folder}/icons/{icon}', f'icons/{icon}')
